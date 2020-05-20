cd45b4730b030edf1d82e883fc03d8db69adf950
e_jescac <e_jescac@mercadona.es>
2020-05-20 23:42:01 +0200

    last changes

--- js/app.js
+++ js/app.js
@@ -13,4 +13,19 @@ $(document).ready(function() {
     //
     // $(".overlay-click").click(clickIconFn);
 
+    console.log('JES amosssss jesidea landing page');
+
+    $.getJSON('data/jesidea-projects.json', function(data) {
+        var items = [];
+        $.each(data, function(key, val) {
+            console.log('JES val-->', val);
+            var extraLink = (val.links.length > 1) ? '<li><a href="'+val.links[1].url+'">'+val.links[1].url+'</a></li>': '';
+            items.push("<li class='simple-list-item' id='" + key + "'><h2>" + val.title + '</h2><img width="100px" src="img/'+val.image+'"/><ul class="right-list"><li><p>'+val.description+'</p></li><li><a href="'+val.links[0].url+'">'+val.links[0].url+'</a></li>'+extraLink+'</ul></li>');
+        });
+
+        $('<ul/>', {
+            class: 'simple-list',
+            html: items.join('')
+        }).appendTo('#container');
+    });
 });
